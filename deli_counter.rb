d3e847563a1f674269255563dfa98bda7bd42be2
Alla Kuzminova <alchik@gmail.com>
2020-09-12 01:22:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def line(kats_deli)
+  people = ""
+  counter = 1;
+  katz_deli.each { |person|
+    counterStr = counter.to_s
+    people << counterStr +". "+person
+    puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    counter += 1
+  }
+  if people.length > 0
+    puts "kats_deli" + katz_deli.to_s
+    puts "The line is currently: "+people
+  else
+    #puts "kats_deli" + katz_deli.to_s
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(kats_deli, name)
+  kats_deli.push(name)
+  index = kats_deli.find_index(name);
+  index += 1
+  puts "Welcome, #{name}. You are number #{index} in line."
+end
+
+def now_serving(kats_deli)
+  currently_serving = kats_deli.pop
+  puts "Currently serving #{currently_serving}."
+end
